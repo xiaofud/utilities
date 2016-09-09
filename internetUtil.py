e52e8ab4fa2ea6b6232852289c11aee101d192b2
hjsmallfly <szu_nwad@icloud.com>
2016-09-09 20:19:10 +0800

    第一次提交: 服务器密码验证, 校园网流量脚本, 代码数量统计脚本

---
+++ internetUtil.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# coding=utf-8
+__author__ = 'smallfly'
+
+import requests
+import re
+
+LOGIN_ADDRESS = "http://1.1.1.2/ac_portal/login.php"
+FLOW_ADDRESS = "http://1.1.1.2/ac_portal/userflux"
+
+def login(username, password, remember=0):
+    data = {
+        "opr": "pwdLogin",
+        "userName": username,
+        "pwd": password,
+        "rememberPwd": remember
+    }
+    resp = requests.post(LOGIN_ADDRESS, data=data)
+    resp.encoding = 'UTF-8'
+    print(resp.status_code, resp.text)
+
+def logout():
+    data = {
+        "opr": "logout"
+    }
+    resp = requests.post(LOGIN_ADDRESS, data=data)
+    resp.encoding = 'UTF-8'
+    print(resp.status_code, resp.text)
+
+def query_quota(display=False):
+    resp = requests.post(FLOW_ADDRESS)
+    resp.encoding = "UTF-8"
+    # print(resp.status_code, resp.text)
+    content = resp.text
+    if "错误" in resp.text:
+        return None, None, None
+    username_pattern = r'<td>(\d*[a-z]+\d*)</td>'
+    username = re.search(username_pattern, content)
+    # print(username.group(1))
+
+    used_flow_pattern = r'(\d*\.?\d*)M'
+    used_flow = re.search(used_flow_pattern, content)
+    # print(used_flow.group(1))
+
+    total_flow_pattern = r'(\d*\.?\d*)G'
+    total_flow = re.search(total_flow_pattern, content)
+    # print(total_flow.group(1))
+
+    user, used, total = username.group(1), used_flow.group(1) , total_flow.group(1)
+    if display:
+        message = "username: {}\nused: {}M\ntotal: {}G\n".format(user, used, total)
+        print(message)
+    return user, used, total
+
+def login_interaction():
+    user = input("username: ")
+    pwd = input("password: ")
+    login(user, pwd)
+    # query_quota()
+
+def interact():
+    choice = input("1. login\n2. logout\n3. query:\n")
+    if not choice in ['1', '2', '3']:
+        return -1
+    return choice
+
+
+def main():
+    choice = interact()
+    if choice == '1':
+        login_interaction()
+        query_quota(display=True)
+    elif choice == '2':
+        logout()
+    elif choice == '3':
+        query_quota(display=True)
+    else:
+        print("fuck u!!!wrong choice")
+        main()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ totalCodeLine.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# coding = utf-8
+"""
+统计代码行数
+"""
+
+import os
+
+def get_path():
+    """
+    输入源文件路径
+    """
+    path = input("enter the root path(default current work dir):\n")
+    if path.strip() == "":
+        path = "."
+    assert os.path.exists(path), "the path does't exist"
+    assert os.path.isdir(path), "the path must be a dir"
+    return path
+
+def get_suffix():
+    """
+    输入源码文件的后缀, 输入以空行结尾
+    """
+    suffix_list = list()
+    while True:
+        suffix = input("input the suffix like .cpp(end input by empty line)\n")
+        suffix = suffix.strip()
+        if suffix == "":
+            break
+        suffix_list.append(suffix)
+    return suffix_list
+        
+def get_line_count(root_path, suffix_list):
+    """
+    得到 root_path 下， 拥有 suffix_list 里面后缀名的源码文件的代码行数量
+    默认不统计空行
+    """
+    #assert len(suffix_list) > 0, "there must be some suffix"
+    line_count = 0
+    empty_line_count = 0
+    for root, dirs, files in os.walk(root_path):
+        for file in files:
+            # print(os.path.abspath(file))
+            # 匹配后缀名
+            file_suffix = os.path.splitext(file)[1]
+            if file_suffix in suffix_list:
+                # 读取该文件
+                with open(os.path.join(root, file)) as f:
+                    for line in f:
+                        if line.strip() == "":
+                            empty_line_count += 1
+                        line_count += 1
+    return line_count, empty_line_count
+
+if __name__ == "__main__":
+    lines, empty = get_line_count(get_path(), get_suffix())
+    print("{}(with empty lines)\n{}(without)\n{}(empty)".format(lines, lines - empty, empty))
+    
+    
